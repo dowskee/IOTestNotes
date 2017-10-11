485cbdc29d5b3509913f3668a3f22ebbfaa2da39
dowskee <31831031+dowskee@users.noreply.github.com>
2017-10-11 13:58:52 -0400

    Notated Example to Keep

---
+++ TestIO/Car.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIO
+{
+
+        class Car
+        {
+            protected string make;
+            protected string model;
+            protected int year;
+            protected double price;
+
+            public string Make //get set for each data piece
+            {
+                set { make = value; }
+                get { return make; }
+            }
+
+            public string Model
+            {
+                set { model = value; }
+                get { return model; }
+            }
+
+            public int Year
+            {
+                set { year = value; }
+                get { return year; }
+            }
+
+            public double Price
+            {
+                set { price = value; }
+                get { return price; }
+            }
+
+            //constructors
+
+            //default constructor
+            public Car() //base constructor? 
+            {
+                make = "";
+                model = "";
+                year = 0;
+                price = 0;
+            }
+
+            //consturctor with values
+            public Car(string CarMake, string CarModel, int CarYear, double CarPrice)
+            {
+                make = CarMake; //calls Make from the array Car
+                model = CarModel;
+                year = CarYear;
+                price = CarPrice;
+            }
+
+            public virtual void PrintInfo()
+            {
+                Console.WriteLine(Make.PadRight(8, ' ') + "\t" + Model.PadRight(8, ' ') + "\t" + Year.ToString().PadRight(8, ' ') + "\t" + "$" + Price.ToString("N2")); //property (capitalized)
+
+                //to enable method override, we must go to the top, in our Person class. 
+                //if the method is defined as virtual, you can override it
+
+            }
+
+            public override string ToString()
+            {
+                return (Make.PadRight(8, ' ') + "\t" + Model.PadRight(8, ' ') + "\t" + Year.ToString().PadRight(8, ' ') + "\t" + "$" + Price.ToString("N2"));
+            }
+
+        }
+    
+}
---
+++ TestIO/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestIO
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //write to a file
+
+            //StreamWriter wr = new StreamWriter("Cars.txt", true); //string path
+
+            //wr.WriteLine("Jeep\tCompass\t2015\t30000"); //writes to file(or application), instead of to console!
+
+            //wr.WriteLine("Chevy\tCruze\t2016\t20000");
+
+            //wr.Close(); //will essentially save your data
+
+            //append by adding "true" after "Cars.txt". This means that every time you add something to the file, it will add it to text file
+
+            //read from a file
+            //once the text is read, it will be split into its proper pieces
+
+            StreamReader rd = new StreamReader("Cars.txt");
+
+            //string str = rd.ReadToEnd(); //this reads the contents of the file
+
+            //Console.WriteLine(str); //prints contents to console
+            List<Car> CarList = new List<Car>();
+            string str = rd.ReadLine(); //ReadLine only reads one line of the file
+            //use a loop to go through these lines one at a time, until we reach the end, which will be a null. 
+
+            while (str != null) //while we still have more lines to read
+            {
+                //Console.WriteLine(str); //print the first/current line
+                
+                string[] tempCar = str.Split(); //This will take the line and split it in to four pieces, for the Car class (Make, Model, Year, Price)
+
+                CarList.Add(new Car(tempCar[0], tempCar[1], int.Parse(tempCar[2]), double.Parse(tempCar[3]))); //this will reference the 
+                //index of each piece(object) in the List array of Car, for Make, Model, Year, and Price. We need to assign value because Car has properties.
+                str = rd.ReadLine(); //will fetch next line. If this is not written, the program will stop. 
+            }
+            //this is good for memory. This helps with efficiency as it breaks it down, instead of ReadToEnd, which dumps it all at once. 
+
+            foreach (Car Element in CarList)
+            {
+                Console.WriteLine(Element.ToString()); //data is coming from your objects
+            }
+
+            //using ORM Frameworks will save you a lot of this code. 
+        }
+    }
+}
